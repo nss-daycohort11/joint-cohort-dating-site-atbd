ddfbbcafc3d460df21167abc22936ec39c77a8b5
Brendon Pierson <brendonpierson@gmail.com>
2015-11-16 10:48:17 -0600

    Added base depencies and grunt tasks

--- javascripts/dating-site.js
+++ javascripts/dating-site.js
@@ -4,11 +4,17 @@ require.config({
     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'firebase': '../lib/bower_components/firebase/firebase',
     'q': '../lib/bower_components/q/q',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'es6': '../lib/bower_components/requirejs-babel/es6',
+    'babel': '../lib/bower_components/requirejs-babel/babel-5.8.22.min'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
